a4aae5b18093bc02fe320d814ea433aa44341a80
tjehdrjs72 <tjehdrjs72@naver.com>
2021-05-25 11:21:11 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- client/src/App.js
+++ client/src/App.js
@@ -13,14 +13,14 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: 'auto'
   },
   table: {
     minWidth: 1080
   },
   progress: {
-    margin: theme.spacing.unit * 2
+    margin: theme.spacing(2)
   }
 })
 
@@ -78,7 +78,7 @@ class App extends Component {
                       key={c.id}
                       id={c.id}
                       image={c.image}
-                      name={c.name}
+                      name={c.NAME}
                       birthday={c.birthday}
                       gender={c.gender}
                       job={c.job}
